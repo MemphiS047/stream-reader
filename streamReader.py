e994d097e878ecbbeb65863560483f9a05789ae1
MemphiS047 <hakverdiyigit@gmail.com>
2022-08-08 00:49:28 +0300

    Added a dummyServer for mimicing server that sends and updated the stream reader function in order to read the data and act accordingly also added the logger as well with date time formatter

--- streamReader.py
+++ streamReader.py
@@ -1,22 +1,31 @@
 import subprocess
-import shlex
 import concurrent.futures
+import logging
+import datetime
+
+
+# Logging configuration
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('MobileControllerJarUpdate.log', 'w', 'utf-8')
+handler.setFormatter(logging.Formatter("%(asctime)s;%(levelname)s;%(message)s"))
+logger.addHandler(handler)
 
 # Reads the stream of data in this case git pull is used for an example
 def read_stream():
-    process = subprocess.Popen(shlex.join(['git', 'pull']), stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(["/home/yigit/dev/github/stream-reader/dummyServer.sh"], stdout=subprocess.PIPE, shell=True)
     while True:
         output = process.stdout.readline()
         if(output == '' and process.poll() is not None):
             break
-        if(b'Already up to date' in output):
-            print("Repository is already up to date")
+        if(b'iteration 2' in output):
+            logging.info('MobileController.jar updated running updateOperation.sh')
             git_update()
     rc = process.poll()
     return rc
 # Runs the update operation shell script which pulls repo then replaces the destinated files
 def git_update():
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        executor.submit(subprocess.Popen(["/home/yigit/dev/github/stream-reader/updateOperation.sh"], stdin=subprocess.PIPE))
+        executor.submit(subprocess.Popen(["/home/yigit/dev/github/stream-reader/updateOperation.sh"], stdin=subprocess.PIPE, shell=True))
 
 read_stream()
--- updateOperation.sh
+++ updateOperation.sh
@@ -1,5 +1,5 @@
-cd "/home/yigit/dev/github/mybs-web-app"
-git status
-cd "/home/yigit/dev/github/stream-reader"
+# cd "/home/yigit/dev/github/mybs-web-app"
+# git status
+# cd "/home/yigit/dev/github/stream-reader"
+# pwd
 cp "/home/yigit/dev/github/stream-reader/dummyDirectory1/temp1.txt" "/home/yigit/dev/github/stream-reader/dummyDirectory2"
-pwd
